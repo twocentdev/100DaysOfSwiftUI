58bbb8a29ee64292cb6167e3d650c414c7ecc4d9
Miguel Angel Sanchez Crespo <miansacre@gmail.com>
2023-12-06 12:34:26 +0100

    Done integer tutorial.

---
+++ day_1/1_3_integers.playground/Contents.swift
@@ -0,0 +1,13 @@
+let baseScore = 0
+let goalScore = 1
+
+var score = baseScore
+print(score)
+score += goalScore
+print(score)
+
+let million = 1_000_000 // "_" is used to print separators into numbers.
+print(million)
+print(million + 1)
+
+print(million.isMultiple(of: 6)) //Like with strings, you can apply functions to integer variables.
