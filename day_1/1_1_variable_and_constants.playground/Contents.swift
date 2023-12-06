fb0c06730fe8af70fc99d21199c7688d7a0423da
Miguel Angel Sanchez Crespo <miansacre@gmail.com>
2023-12-06 12:33:44 +0100

    Done variables and constants tutorial.

---
+++ day_1/1_1_variable_and_constants.playground/Contents.swift
@@ -0,0 +1,10 @@
+var greeting = "Hello," //Define a variable
+greeting += " John" //Update a variable
+print(greeting)
+var name = " and Daphne!"
+greeting += name
+print(greeting)
+
+let pi = 3.14 //Define a constant
+//pi = pi * 2 //Cannot 'update' a constant
+print(pi)
