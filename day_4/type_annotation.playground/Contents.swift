76aef8f06f84080e1a219002881b9917b2e0e540
Miguel Angel Sanchez Crespo <miansacre@gmail.com>
2023-12-22 07:50:35 +0100

    Done all tutorials for day 4.

---
+++ day_4/type_annotation.playground/Contents.swift
@@ -0,0 +1,12 @@
+let name = "Ted"
+let surname: String = "Lasso" //This way allows to set explicitly the type a variable is.
+var score: Double = 0 //If not used the type notation on this var, it would be defined as Int.
+
+let luckyNumber: Int = 13
+let isAuth: Bool = true
+var albums: [String] = ["Red", "Fearless"] //This is how you define an Array with a given element type.
+var teams: [String] = [String]() //This are some extra ways to define an Array.
+var cities: [String] = []
+var clues = [String]()
+var user: [String: String] = ["id": "@twostraws"] //This is how you define a Dict.
+var books: Set<String> = Set(["The Bluest Eye", "Foundation", "Girl, Woman, Other"]) //This is how you define a Set.
