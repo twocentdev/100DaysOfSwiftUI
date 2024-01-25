8065aa0e32d592de059116531bc3d0040cf113c2
Miguel Angel Sanchez Crespo <miansacre@gmail.com>
2024-01-25 09:10:09 +0100

    Created empty project

---
+++ day_25/RockPaperScissorsGame/RockPaperScissorsGame/RockPaperScissorsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsGameApp.swift
+//  RockPaperScissorsGame
+//
+//  Created by Miguel Sánchez Crespo on 25/1/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
